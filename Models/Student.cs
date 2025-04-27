b1f30c607ca65ca35bb09919c5c7f2c59c2e5a10
Yogesh Patil <patilyoge625@gmail.com>
2025-04-27 13:13:17 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudMVC8.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Email { get; set; }
+}
---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMVC8.Models;
+
+public partial class StudentDbContext : DbContext
+{
+    public StudentDbContext()
+    {
+    }
+
+    public StudentDbContext(DbContextOptions<StudentDbContext> options)
+        : base(options)
+    {
+    }
+
+    
+    public virtual DbSet<Student> Students { get; set; }
+
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-KRUVNM2M\\SQLEXPRESS01;Database=StudentDB;Trusted_Connection=True;Encrypt=False;");
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+    
+            entity.HasKey(e => e.Id).HasName("PK__Students__3214EC07B2B478E5");
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
